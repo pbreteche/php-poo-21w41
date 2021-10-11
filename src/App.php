d7e094d38594f37f0ae426f99c85dc23c2ee6a96
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-11 12:32:24 +0200

    Utilisation des donées de requête dans la réponse

--- src/App.php
+++ src/App.php
@@ -7,7 +7,10 @@ class App
     public function handleRequest($request)
     {
         $response = new Response();
-        $response->setBody('<h1>Bonjour !</h1>');
+        $response->setBody(
+            '<h1>Bonjour !</h1>'.
+            '<p>L\'URL demandée est '.$request->getUri().'</p>'
+        );
 
         return $response;
     }
