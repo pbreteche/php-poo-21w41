e90a62250a6b6484ccc21123e4cd810fd04ada7a
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-12 14:13:44 +0200

    Factorisation

--- src/ContactLoader.php
+++ src/ContactLoader.php
@@ -4,17 +4,28 @@ namespace Dawan;
 
 class ContactLoader
 {
+    private $allContacts;
+
     public function load()
     {
-        return json_decode(file_get_contents(__DIR__.'/../data/contacts.json'));
+        return $this->getAllContacts();
     }
 
     public function loadById(int $id)
     {
-        $data = json_decode(file_get_contents(__DIR__.'/../data/contacts.json'));
+        $data = $this->getAllContacts();
 
         $dataById = array_column($data, null, 'id');
 
         return $dataById[$id] ?? null;
+    }
+
+    private function getAllContacts()
+    {
+        if (!$this->allContacts) {
+            $this->allContacts = json_decode(file_get_contents(__DIR__.'/../data/contacts.json'));            
+        }
+
+        return $this->allContacts;
     }
 }
